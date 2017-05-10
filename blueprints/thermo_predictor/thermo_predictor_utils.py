2ff600d123d7f1467c7ca2defbac59a22cb29ef4
KEHANG <kehanghan@gmail.com>
2017-05-10 10:47:46 -0400

    move thermo_predictor.py as a utils for thermo_predictor blueprint

--- blueprints/thermo_predictor/thermo_predictor_utils.py
+++ blueprints/thermo_predictor/thermo_predictor_utils.py
@@ -1,27 +1,27 @@
-
 import os
-
 
 from rmgpy.molecule import Molecule
 from rmgpy.cnn_framework.predictor import Predictor
 
-
 def load_cnn_thermo_predictor(thermo_predictor_model='zb_biycyclics_t.r.0.5'):
 
-	datasets_file = os.path.join('data',
-								'thermo_predictor',
+	thermo_predictor_data = os.path.join(os.path.dirname(__file__),
+										'data')
+
+	datasets_file = os.path.join(thermo_predictor_data,
+								'predictor_models',
 								thermo_predictor_model,
 								'datasets.txt')
 
-	predictor_input = os.path.join('data',
-								'thermo_predictor',
+	predictor_input = os.path.join(thermo_predictor_data,
+								'predictor_models',
 								thermo_predictor_model,
 								'predictor_input.py')
 
 	h298_predictor = Predictor(datasets_file=datasets_file)
 	h298_predictor.load_input(predictor_input)
-	param_path = os.path.join('data',
-							'thermo_predictor',
+	param_path = os.path.join(thermo_predictor_data,
+							'predictor_models',
 							thermo_predictor_model,
 							'saved_model',
 							'full_train.h5')
