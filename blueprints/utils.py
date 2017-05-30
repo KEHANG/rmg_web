34823395dc7b84b7790ae18cb95f41b5d9d43df5
KEHANG <kehanghan@gmail.com>
2017-05-30 16:02:47 -0400

    add helper methods to get authentication info from config file

--- blueprints/utils.py
+++ blueprints/utils.py
@@ -1,19 +1,51 @@
 import os
+import ConfigParser
 
 from rmgpy.molecule import Molecule
+
+def read_config(cfg_path='default'):
+	'''This function reads a configuration file and returns an equivalent dictionary'''
+
+	config_parser = ConfigParser.SafeConfigParser()
+	config_parser.optionxform = str
+
+	if cfg_path == 'default':
+		cfg_path = os.path.join(os.path.dirname(__file__), 'config.cfg')
+	with open(cfg_path, 'r') as fid:
+		config_parser.readfp(fid)
+	return config_parser._sections
+
+def getTCDAuthenticationInfo():
+	
+	try:
+		config = read_config()
+
+		host = config['ThermoCentralDatabase']['TCD_HOST']
+		port = int(config['ThermoCentralDatabase']['TCD_PORT'])
+		username = config['ThermoCentralDatabase']['TCD_USER']
+		password = config['ThermoCentralDatabase']['TCD_PW']
+
+		return host, port, username, password
+	except KeyError:
+		print('Thermo Central Database Configuration File  Not Completely Set!')
+ 
+	return 'None', 0, 'None', 'None'
 
 def getPPDAuthenticationInfo():
 
 	try:
-		host = os.environ['PPD_HOST']
-		port = int(os.environ['PPD_PORT'])
-		username = os.environ['PPD_USER']
-		password = os.environ['PPD_PW']
+		config = read_config()
+
+		host = config['PredictorPerformanceDatabase']['PPD_HOST']
+		port = int(config['PredictorPerformanceDatabase']['PPD_PORT'])
+		username = config['PredictorPerformanceDatabase']['PPD_USER']
+		password = config['PredictorPerformanceDatabase']['PPD_PW']
+
+		return host, port, username, password
 	except KeyError:
 		print('Predictor Performance Database Authentication Environment Variables Not Completely Set!')
-		return 'None', 0, 'None', 'None'
-
-	return host, port, username, password
+	
+	return 'None', 0, 'None', 'None'
 
 def connect_to_PPD():
 
