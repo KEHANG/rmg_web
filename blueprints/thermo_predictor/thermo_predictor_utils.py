de6949c96e68ae14604008536cc8a6a58e506998
KEHANG <kehanghan@gmail.com>
2017-08-08 13:03:56 -0400

    tab to space conversion (not important)

--- blueprints/thermo_predictor/thermo_predictor_utils.py
+++ blueprints/thermo_predictor/thermo_predictor_utils.py
@@ -5,27 +5,28 @@ from rmgpy.cnn_framework.predictor import Predictor
 
 def load_cnn_thermo_predictor(thermo_predictor_model='zb_biycyclics_t.r.0.5'):
 
-	thermo_predictor_data = os.path.join(os.path.dirname(__file__),
-										'data')
+    thermo_predictor_data = os.path.join(os.path.dirname(__file__),
+                                        'data')
 
-	datasets_file = os.path.join(thermo_predictor_data,
-								'predictor_models',
-								thermo_predictor_model,
-								'datasets.txt')
+    datasets_file = os.path.join(thermo_predictor_data,
+                                'predictor_models',
+                                thermo_predictor_model,
+                                'datasets.txt')
 
-	predictor_input = os.path.join(thermo_predictor_data,
-								'predictor_models',
-								thermo_predictor_model,
-								'predictor_input.py')
+    predictor_input = os.path.join(thermo_predictor_data,
+                                'predictor_models',
+                                thermo_predictor_model,
+                                'predictor_input.py')
 
-	h298_predictor = Predictor(datasets_file=datasets_file)
-	h298_predictor.load_input(predictor_input)
-	param_path = os.path.join(thermo_predictor_data,
-							'predictor_models',
-							thermo_predictor_model,
-							'saved_model',
-							'full_train.h5')
-	h298_predictor.load_parameters(param_path)
+    h298_predictor = Predictor(datasets_file=datasets_file)
+    h298_predictor.load_input(predictor_input)
+    param_path = os.path.join(thermo_predictor_data,
+                            'predictor_models',
+                            thermo_predictor_model,
+                            'saved_model',
+                            'full_train.h5')
+    h298_predictor.load_parameters(param_path)
 
-	return h298_predictor
+    return h298_predictor
+
 
